0c604dc14c02bd658c7a88cd79883ad8cac214ab
Matthew Churilla <ltguide@gmail.com>
2012-01-21 16:16:51 -0500

    fix chunks not unloading on save

--- src/ltguide/minebackup/CallSync.java
+++ src/ltguide/minebackup/CallSync.java
@@ -2,6 +2,10 @@ package ltguide.minebackup;
 
 import java.util.concurrent.Callable;
 
+import ltguide.base.Base;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
@@ -9,6 +13,8 @@ public class CallSync implements Callable<Boolean> {
 	private final MineBackup plugin;
 	private final String action;
 	private final World world;
+	private int x;
+	private int z;
 	
 	public CallSync(final MineBackup plugin, final String action, final World world) {
 		this.plugin = plugin;
@@ -21,10 +27,26 @@ public class CallSync implements Callable<Boolean> {
 			for (final Player player : world.getPlayers())
 				player.saveData();
 			
+			final Location spawn = world.getSpawnLocation();
+			x = (int) spawn.getX() >> 4;
+			z = (int) spawn.getZ() >> 4;
+			
+			int chunks = 0;
+			for (final Chunk chunk : world.getLoadedChunks())
+				if (!keepChunk(chunk) && world.unloadChunk(chunk.getX(), chunk.getZ(), true, true)) chunks++;
+			
+			Base.debug("\t| unloaded " + chunks + " chunks @ " + Base.stopTime());
+			
 			world.save();
 		}
 		else if ("count".equals(action)) return plugin.persist.hasPlayers(world);
 		
 		return false;
+	}
+	
+	private boolean keepChunk(final Chunk chunk) {
+		if (!world.getKeepSpawnInMemory()) return false;
+		
+		return x - 13 <= chunk.getX() && chunk.getX() <= x + 13 && z - 13 <= chunk.getZ() && chunk.getZ() <= z + 13;
 	}
 }
